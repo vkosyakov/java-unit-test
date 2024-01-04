3cdb92ed79a658bb96d35993572028453f10cce7
VLAD <kosyackoff.vlad@yandex.ru>
2024-01-04 18:09:33 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,7 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	// Напиши код здесь
+    assertEquals("Пользователю больше 18",true,isAdult);
+}
---
+++ task_3.java
@@ -0,0 +1,38 @@
+// Напиши аннотацию для параметризованных тестов
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	this.age = age;
+    this.result = result;
+      // Инициализируй поля класса в конструкторе
+        
+  }
+
+  @Parameterized.Parameters
+ // Пометь метод аннотацией для параметров
+  public static Object[][] getTextData() {
+	return new Object[][] {
+        {17,false},
+        {18,true},
+        {20,true},
+        {21,true},// Заполни массив тестовыми данными и ожидаемым результатом
+    };
+  }
+
+  @Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+      // Передай сюда возраст пользователя
+    boolean isAdult = program.checkIsAdult(age);
+	// Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+    assertEquals("Ошибка! Пользователь несовершеннолетний",result,isAdult);
+	}
+}
